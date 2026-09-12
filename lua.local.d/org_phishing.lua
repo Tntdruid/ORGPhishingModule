cb0d4c771556106935e5f1ebf9ad0630e86a7773
Christian <wall@clanzone.dk>
2026-09-12 18:57:56 +0200

    Rebuild ORG_PHISHING with brand definitions and patterns

--- lua.local.d/org_phishing.lua
+++ lua.local.d/org_phishing.lua
@@ -3,7 +3,6 @@
 -- Compatible with Rspamd 4.1.5
 
 local logger = require "rspamd_logger"
-
 logger.infox("ORG_PHISHING: module loaded")
 
 ---------------------------------------------------------------------------
@@ -35,11 +34,14 @@ local function domain_matches(domain, list)
 end
 
 ---------------------------------------------------------------------------
--- BRAND DEFINITIONS
+-- BRAND DEFINITIONS (FULL SET)
 ---------------------------------------------------------------------------
 
 local brands = {
 
+  ---------------------------------------------------------------------------
+  -- YOUSEE
+  ---------------------------------------------------------------------------
   YOUSEE = {
     symbol = "ORG_PHISHING_YOUSEE",
     score = 8.0,
@@ -59,6 +61,9 @@ local brands = {
     }
   },
 
+  ---------------------------------------------------------------------------
+  -- POSTNORD
+  ---------------------------------------------------------------------------
   POSTNORD = {
     symbol = "ORG_PHISHING_POSTNORD",
     score = 8.0,
@@ -66,15 +71,21 @@ local brands = {
     domains = {
       "postnord.dk","postnord.com",
       "sendgrid.net","postnord.%w+%.sendgrid%.net",
-      "trk.postnord.com","m.postnord.com"
+      "trk.postnord.com","m.postnord.com",
+      "postnord-secure.com","postnord-delivery.com",
+      "postnord-update.com","postnord-verify.com"
     },
     urgency = {
-      "din pakke er tilbageholdt",
-      "betal gebyr for levering",
-      "leveringsproblem"
+      "din postnord pakke er tilbageholdt",
+      "postnord levering afventer betaling",
+      "postnord pakke mangler information",
+      "verify your postnord delivery"
     }
   },
 
+  ---------------------------------------------------------------------------
+  -- COOP
+  ---------------------------------------------------------------------------
   COOP = {
     symbol = "ORG_PHISHING_COOP",
     score = 6.0,
@@ -90,6 +101,9 @@ local brands = {
     }
   },
 
+  ---------------------------------------------------------------------------
+  -- NETFLIX
+  ---------------------------------------------------------------------------
   NETFLIX = {
     symbol = "ORG_PHISHING_NETFLIX",
     score = 8.0,
@@ -97,15 +111,23 @@ local brands = {
     domains = {
       "netflix.com",
       "mandrillapp.com","sendgrid.net","sfmc-email.com",
-      "email.netflix.com","m.netflix.com"
+      "email.netflix.com","m.netflix.com",
+      "netflix-secure.com","netflix-billing.com",
+      "netflix-update.com","netflix-verify.com"
     },
     urgency = {
       "din netflix betaling er afvist",
+      "netflix betaling mangler",
+      "din netflix konto er låst",
       "netflix abonnement udløber",
+      "update your netflix payment",
       "verify your netflix account"
     }
   },
 
+  ---------------------------------------------------------------------------
+  -- MOBILEPAY / MITID (EASYBANK)
+  ---------------------------------------------------------------------------
   EASYBANK = {
     symbol = "ORG_PHISHING_EASYBANK",
     score = 9.0,
@@ -124,22 +146,18 @@ local brands = {
     }
   },
 
-  -- NEW: EasyPark spoof protection (examsaide + Amazon SES)
+  ---------------------------------------------------------------------------
+  -- EASYPARK
+  ---------------------------------------------------------------------------
   EASYPARK = {
     symbol = "ORG_PHISHING_EASYPARK",
     score = 8.0,
     keywords = { "easypark", "easy park", "easy-park" },
     domains = {
-      -- legit EasyPark domains
       "easypark.dk","easypark.se","easypark.no","easypark.fi","easypark.net",
       "easyparkapp.com","easyparkgroup.com",
-
-      -- typical ESPs they might use
       "sendgrid.net","mandrillapp.com","sfmc-email.com",
-
-      -- spoof infra seen in 2026 campaigns
-      "examsaide.com",
-      "amazonses.com",
+      "examsaide.com","amazonses.com",
       "eu-central-1.amazonses.com",
       "smtp-out.eu-central-1.amazonses.com",
       "b224-6.smtp-out.eu-central-1.amazonses.com"
@@ -150,6 +168,138 @@ local brands = {
       "betaling for parkering mangler",
       "din parkering er ugyldig",
       "verify your easypark account"
+    }
+  },
+
+  ---------------------------------------------------------------------------
+  -- KLARNA
+  ---------------------------------------------------------------------------
+  KLARNA = {
+    symbol = "ORG_PHISHING_KLARNA",
+    score = 9.0,
+    keywords = {
+      "klarna","klarna betaling","klarna pay",
+      "klarna invoice","klarna faktura","klarna konto"
+    },
+    domains = {
+      "klarna.com","klarna.dk","klarna.se","klarna.no","klarna.fi",
+      "klarna.de","klarna.co.uk",
+      "email.klarna.com","m.klarna.com",
+      "klarna.mkt-mail.com","klarna.mkt-cloud.com",
+      "klarnapay.com","klarnasecure.com","klarnaupdate.com",
+      "klarnaverify.com","klarna-check.com","klarna-billing.com"
+    },
+    urgency = {
+      "din klarna betaling er afvist",
+      "klarna betaling mangler",
+      "din klarna faktura er udløbet",
+      "verify your klarna account",
+      "update your klarna payment",
+      "klarna security update",
+      "klarna konto låst"
+    }
+  },
+
+  ---------------------------------------------------------------------------
+  -- DAO
+  ---------------------------------------------------------------------------
+  DAO = {
+    symbol = "ORG_PHISHING_DAO",
+    score = 8.0,
+    keywords = {
+      "dao","dao365","dao 365",
+      "dao levering","dao forsendelse","dao tracking"
+    },
+    domains = {
+      "dao.as","dao365.dk","dao.asia","dao.asn","dao.asn.dk",
+      "dao.asn.email","dao.asn.delivery","dao.asn.services",
+      "email.dao.as","trk.dao.as","m.dao.as",
+      "dao.mkt-mail.com","dao.mkt-cloud.com",
+      "dao-secure.com","dao-delivery.com","dao-update.com",
+      "dao-verify.com","dao-tracking.com","dao365-secure.com"
+    },
+    urgency = {
+      "din dao levering er tilbageholdt",
+      "dao levering afventer betaling",
+      "dao pakke mangler information",
+      "verify your dao delivery",
+      "dao security update",
+      "dao konto låst"
+    }
+  },
+
+  ---------------------------------------------------------------------------
+  -- GLS
+  ---------------------------------------------------------------------------
+  GLS = {
+    symbol = "ORG_PHISHING_GLS",
+    score = 8.0,
+    keywords = {
+      "gls","gls pakke","gls levering","gls forsendelse","gls tracking"
+    },
+    domains = {
+      "gls.dk","gls-group.eu","gls.de","gls.nl","gls.at","gls.it",
+      "email.gls.dk","trk.gls.dk","m.gls.dk",
+      "sendgrid.net","mandrillapp.com",
+      "gls-delivery.com","gls-secure.com","gls-update.com",
+      "gls-verify.com","gls-pakke.com"
+    },
+    urgency = {
+      "din gls pakke er tilbageholdt",
+      "gls levering afventer betaling",
+      "gls pakke mangler information",
+      "verify your gls delivery",
+      "gls security update"
+    }
+  },
+
+  ---------------------------------------------------------------------------
+  -- DHL
+  ---------------------------------------------------------------------------
+  DHL = {
+    symbol = "ORG_PHISHING_DHL",
+    score = 8.5,
+    keywords = {
+      "dhl","dhl express","dhl pakke","dhl tracking"
+    },
+    domains = {
+      "dhl.com","dhl.de","dhl.dk","dhl.se","dhl.fi","dhl.no",
+      "email.dhl.com","trk.dhl.com","m.dhl.com",
+      "sendgrid.net","mandrillapp.com",
+      "dhl-secure.com","dhl-delivery.com","dhl-update.com",
+      "dhl-verify.com","dhl-pakke.com"
+    },
+    urgency = {
+      "din dhl pakke er tilbageholdt",
+      "dhl levering afventer betaling",
+      "dhl shipment on hold",
+      "verify your dhl delivery",
+      "dhl security update"
+    }
+  },
+
+  ---------------------------------------------------------------------------
+  -- FEDEX
+  ---------------------------------------------------------------------------
+  FEDEX = {
+    symbol = "ORG_PHISHING_FEDEX",
+    score = 8.0,
+    keywords = {
+      "fedex","fed ex","fedex tracking","fedex shipment"
+    },
+    domains = {
+      "fedex.com","fedex.com.cn","fedex.com.au","fedex.com.uk",
+      "email.fedex.com","trk.fedex.com","m.fedex.com",
+      "sendgrid.net","mandrillapp.com",
+      "fedex-secure.com","fedex-delivery.com","fedex-update.com",
+      "fedex-verify.com","fedex-shipment.com"
+    },
+    urgency = {
+      "your fedex package is on hold",
+      "fedex shipment requires payment",
+      "fedex delivery pending",
+      "verify your fedex shipment",
+      "fedex security update"
     }
   },
 }
